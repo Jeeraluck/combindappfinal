49f5951042e9e23459ddc04a2234c08534feb2e7
unknown <jeeraluck.sukmak@gmail.com>
2018-07-01 16:54:03 +0700

    Commbind menu complete

---
+++ src/app/app.module.ts
@@ -0,0 +1,60 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule, Tabs } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { TabHomePage } from '../pages/tab-home/tab-home';
+import { TabCoursePage } from '../pages/tab-course/tab-course';
+import { TabServicePage } from '../pages/tab-service/tab-service';
+import { TabActiclePage } from '../pages/tab-acticle/tab-acticle';
+import { TabContactPage } from '../pages/tab-contact/tab-contact';
+import { SidePaymentPage } from '../pages/side-payment/side-payment';
+import { SidePortfolioPage } from '../pages/side-portfolio/side-portfolio';
+import { SideSchedulePage } from '../pages/side-schedule/side-schedule';
+import { SideSettingPage } from '../pages/side-setting/side-setting';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabHomePage,
+    TabCoursePage,
+    TabServicePage,
+    TabActiclePage,
+    TabContactPage,
+    SidePaymentPage,
+    SidePortfolioPage,
+    SideSchedulePage,
+    SideSettingPage,
+    TabsPage,
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabHomePage,
+    TabCoursePage,
+    TabServicePage,
+    TabActiclePage,
+    TabContactPage,
+    SidePaymentPage,
+    SidePortfolioPage,
+    SideSchedulePage,
+    SideSettingPage,
+    TabsPage,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
